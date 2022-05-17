ed37d583e1f97f0f920959710bdcca373269986f
Craig <73821646+Stupak100@users.noreply.github.com>
2022-05-17 22:30:00 +0200

    Update and rename Bąbelki.cpp to Bubble sort.cpp

--- Bubble sort.cpp
+++ Bubble sort.cpp
@@ -1,17 +1,18 @@
-//include necessary libraries and std
+///include necessary libraries and std
 #include<iostream>
 using namespace std;
 
 //define array and other necessary variables
 const int N = 10;
-int tab[N];
+float tab[N];
 int  i;
 bool sorted = false;
 
 //main code
 int main() {
 	//input into array
-	for (int i = 0; i < N; i++) {
+        cout<<"Input 10 numbers\n"
+	for (i = 0; i < N; i++) {
 		cin >> tab[i];
 	}
 
@@ -19,16 +20,16 @@ int main() {
 	while (sorted == false) {
 		sorted = true;
 		for (i = 0; i < N; i++) {
-			if (tab[i] > tab[i + 1]) {
+			if (tab[i - 1] > tab[i]) {
 				sorted = false;
-				swap(tab[i], tab[i + 1]);
+				swap(tab[i - 1], tab[i]);
 			}
 		}
 	}
 
 	//output from array
 	for (int i = 0; i < N; i++) {
-		cout << tab[i]<<", ";
+		cout << tab[i] << ", ";
 	}
 	return 0;
 }
